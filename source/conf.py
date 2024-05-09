dc92e176b0a259df41bb94d63864dc30ad827f28
Kazuya Takei <myself@attakei.net>
2024-05-10 03:37:22 +0900

    feat: Version selector

--- source/conf.py
+++ source/conf.py
@@ -1,59 +1,45 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
+# -- Project information
 project = "sphinx-revealjs on RTD"
 copyright = "2021, Kazuya Takei"
 author = "Kazuya Takei"
+release = "2024.5.10"
 
-# The full version, including alpha/beta/rc tags
-release = "2021.9.18"
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# -- General configuration
 extensions = [
     "sphinx.ext.todo",
     "sphinx_revealjs",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# -- For extension
+# - sphinx_revealjs
+revealjs_html_theme = "revealjs-simple"
+revealjs_static_path = ["_static"]
+revealjs_script_conf = {
+    "controls": True,
+    "progress": True,
+    "hash": True,
+    "center": True,
+    "transition": "slide",
+}
+revealjs_script_plugins = [
+    {
+        "name": "RevealCustomControls",
+        "src": "https://cdn.jsdelivr.net/npm/reveal.js-plugins@latest/customcontrols/plugin.js",
+    },
+]
+revealjs_js_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js",
+    "rtd-badge.js",
+]
+revealjs_css_files = [
+    "revealjs/plugin/highlight/zenburn.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+    "https://cdn.jsdelivr.net/npm/reveal.js-plugins@latest/customcontrols/style.css",
+    "rtd-badge.css",
+]
 
 
 def override_builder(app, config):
@@ -71,5 +57,17 @@ def override_builder(app, config):
     # app.registry.builders[override_target] = app.registry.builders["revealjs"]
 
 
+def _inject_rtd_version(app, pagename, templatename, context, doctree):
+    if not doctree:
+        return
+    context["READTHEDOCS"] = True
+    if "READTHEDOCS" in context:
+        output = app.builder.templates.render("_rtd-versions.html", context)
+        context["revealjs_page_confs"].append(
+            {"customcontrols": {"controls": [{"icon": output}]}}
+        )
+
+
 def setup(app):
     app.connect("config-inited", override_builder)
+    app.connect("html-page-context", _inject_rtd_version)
